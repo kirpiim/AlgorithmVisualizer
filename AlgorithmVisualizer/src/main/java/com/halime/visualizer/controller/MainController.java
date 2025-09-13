0ea6317f30f72254410a2839aa523c2d53605da1
Your Name <your@email.com>
2025-09-13 22:44:00 +0800

    feat(controller): hook up DFS button to run DFS visualization

--- AlgorithmVisualizer/src/main/java/com/halime/visualizer/controller/MainController.java
+++ AlgorithmVisualizer/src/main/java/com/halime/visualizer/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.halime.visualizer.controller;
 
 import com.halime.visualizer.algorithm.BFS;
+import com.halime.visualizer.algorithm.DFS;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
@@ -21,12 +22,13 @@ public class MainController {
         bfs.run(gc, speed);
     }
 
-
-
-
     @FXML
     private void handleRunDFS() {
         statusLabel.setText("Running DFS...");
+        var gc = canvas.getGraphicsContext2D();
+        double speed = Math.max(1, speedSlider.getValue());
+        DFS dfs = new DFS();
+        dfs.run(gc, speed);
     }
 
     @FXML
